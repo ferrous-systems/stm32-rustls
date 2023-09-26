280d031af2efb9de967a53025233c18ca45e9e71
aissata <aimaiga2@gmail.com>
2023-09-26 13:41:04 +0200

    Vec and Arc seem to work

--- src/bin/global_alloc.rs
+++ src/bin/global_alloc.rs
@@ -2,12 +2,14 @@
 #![no_std]
 
 extern crate alloc;
+use alloc::sync::Arc;
 use alloc::vec::Vec;
+
 use core::mem::MaybeUninit;
 use embedded_alloc::Heap;
 
 use f429 as _;
-use stm32f4xx_hal::{pac, prelude::*}; // global logger + panicking-behavior + memory layout
+use stm32f4xx_hal::pac; // global logger + panicking-behavior + memory layout
 
 const HEAP_SIZE: usize = 1024;
 #[global_allocator]
@@ -16,8 +18,6 @@ static HEAP: Heap = Heap::empty();
 #[cortex_m_rt::entry]
 fn main() -> ! {
     defmt::info!("Hello Alloc!");
-    let p = pac::Peripherals::take().unwrap();
-
     {
         static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
         unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
@@ -25,7 +25,9 @@ fn main() -> ! {
 
     let mut xs = Vec::new();
     xs.push(78);
+    let arc_xs_a: Arc<Vec<i32>> = Arc::new(xs);
+    let arc_xs_b: Arc<Vec<i32>> = arc_xs_a.clone();
 
-    defmt::info!("{:?}", xs[0]);
+    defmt::info!("arc_xs_b[0]: {:?}", arc_xs_b[0]);
     loop {}
 }

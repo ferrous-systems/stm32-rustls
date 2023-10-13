cc614cdda624c497f1e6ce13f21c59604c78560c
aissata <aimaiga2@gmail.com>
2023-10-13 09:54:44 +0200

    global alloc on tcp client

---
+++ stm32f4/src/bin/tcp_client_alloc.rs
@@ -0,0 +1,125 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+extern crate alloc;
+use alloc::vec;
+use core::mem::MaybeUninit;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_net::{Ipv4Address, Stack, StackResources};
+use embassy_stm32::eth::generic_smi::GenericSMI;
+use embassy_stm32::eth::{Ethernet, PacketQueue};
+use embassy_stm32::peripherals::ETH;
+use embassy_stm32::rng::Rng;
+use embassy_stm32::time::mhz;
+use embassy_stm32::{bind_interrupts, eth, peripherals, rng, Config};
+use embassy_time::Duration;
+use embassy_time::Timer;
+use embedded_alloc::Heap;
+
+use static_cell::make_static;
+use {defmt_rtt as _, panic_probe as _};
+
+const HEAP_SIZE: usize = 1024;
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+static START: spin::Once = spin::Once::new();
+
+pub fn init_heap() {
+    START.call_once(|| {
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe {
+            HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE);
+        }
+    });
+}
+
+bind_interrupts!(struct Irqs {
+    ETH => eth::InterruptHandler;
+    HASH_RNG => rng::InterruptHandler<peripherals::RNG>;
+});
+
+type Device = Ethernet<'static, ETH, GenericSMI>;
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<Device>) -> ! {
+    stack.run().await
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) -> ! {
+    let mut config = Config::default();
+    config.rcc.sys_ck = Some(mhz(100));
+    let p = embassy_stm32::init(config);
+
+    info!("Hello World!");
+
+    // Generate random seed.
+    let mut rng = Rng::new(p.RNG, Irqs);
+    let mut seed = [0; 8];
+    let _ = rng.async_fill_bytes(&mut seed).await;
+    let seed = u64::from_le_bytes(seed);
+
+    let mac_addr = [0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF];
+
+    let device = Ethernet::new(
+        make_static!(PacketQueue::<16, 16>::new()),
+        p.ETH,
+        Irqs,
+        p.PA1,
+        p.PA2,
+        p.PC1,
+        p.PA7,
+        p.PC4,
+        p.PC5,
+        p.PG13,
+        p.PB13,
+        p.PG11,
+        GenericSMI::new(0),
+        mac_addr,
+    );
+
+    let config = embassy_net::Config::dhcpv4(Default::default());
+
+    // Init network stack
+    let stack = &*make_static!(Stack::new(
+        device,
+        config,
+        make_static!(StackResources::<2>::new()),
+        seed
+    ));
+
+    // Launch network task
+    unwrap!(spawner.spawn(net_task(&stack)));
+
+    info!("Network task initialized");
+
+    // Then we can use it!
+    let mut rx_buffer = [0; 4096];
+    let mut tx_buffer = [0; 4096];
+
+    // send a message
+    init_heap();
+    let msg = vec![104, 101, 108, 108, 111];
+
+    loop {
+        let mut socket = embassy_net::tcp::TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+        socket.set_timeout(Some(Duration::from_secs(1000)));
+        let add = "192.168.50.67".parse::<Ipv4Address>().unwrap();
+        info!("Listening on TCP:1234...");
+        if let Err(e) = socket.connect((add, 1234)).await {
+            warn!("connect error: {:?}", e);
+            Timer::after(Duration::from_secs(3)).await;
+            continue;
+        }
+        info!("Connected to {:?}", socket.remote_endpoint());
+        loop {
+            if let Err(e) = socket.write(&msg).await {
+                warn!("write error: {:?}", e);
+                break;
+            }
+            info!("txd: {}", core::str::from_utf8(&msg).unwrap());
+            Timer::after(Duration::from_secs(10)).await;
+        }
+    }
+}

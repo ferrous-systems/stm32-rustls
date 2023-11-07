6de9fff366536088f6c18f781b220daf6632055c
aissata <aimaiga2@gmail.com>
2023-11-07 11:22:53 +0100

    getrandom conflict

---
+++ stm32f429/src/aead.rs
@@ -0,0 +1,100 @@
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+
+use chacha20poly1305::{AeadInPlace, KeyInit};
+use rustls::crypto::cipher::{self, AeadKey, Iv, UnsupportedOperationError, NONCE_LEN};
+use rustls::ConnectionTrafficSecrets;
+
+pub struct Chacha20Poly1305;
+
+impl cipher::Tls12AeadAlgorithm for Chacha20Poly1305 {
+    fn encrypter(
+        &self,
+        key: cipher::AeadKey,
+        iv: &[u8],
+        _: &[u8],
+    ) -> Box<dyn cipher::MessageEncrypter> {
+        Box::new(Tls12Cipher(
+            chacha20poly1305::ChaCha20Poly1305::new_from_slice(key.as_ref()).unwrap(),
+            cipher::Iv::copy(iv),
+        ))
+    }
+
+    fn decrypter(&self, key: cipher::AeadKey, iv: &[u8]) -> Box<dyn cipher::MessageDecrypter> {
+        Box::new(Tls12Cipher(
+            chacha20poly1305::ChaCha20Poly1305::new_from_slice(key.as_ref()).unwrap(),
+            cipher::Iv::copy(iv),
+        ))
+    }
+
+    fn key_block_shape(&self) -> cipher::KeyBlockShape {
+        cipher::KeyBlockShape {
+            enc_key_len: 32,
+            fixed_iv_len: 12,
+            explicit_nonce_len: 0,
+        }
+    }
+
+    fn extract_keys(
+        &self,
+        key: AeadKey,
+        iv: &[u8],
+        _explicit: &[u8],
+    ) -> Result<ConnectionTrafficSecrets, UnsupportedOperationError> {
+        // This should always be true because KeyBlockShape and the Iv nonce len are in agreement.
+        debug_assert_eq!(NONCE_LEN, iv.len());
+        Ok(ConnectionTrafficSecrets::Chacha20Poly1305 {
+            key,
+            iv: Iv::new(iv[..].try_into().unwrap()),
+        })
+    }
+}
+
+struct Tls12Cipher(chacha20poly1305::ChaCha20Poly1305, cipher::Iv);
+
+impl cipher::MessageEncrypter for Tls12Cipher {
+    fn encrypt(
+        &self,
+        m: cipher::BorrowedPlainMessage,
+        seq: u64,
+    ) -> Result<cipher::OpaqueMessage, rustls::Error> {
+        let total_len = m.payload.len() + CHACHAPOLY1305_OVERHEAD;
+
+        let mut payload = Vec::with_capacity(total_len);
+        payload.extend_from_slice(m.payload);
+
+        let nonce = chacha20poly1305::Nonce::from(cipher::Nonce::new(&self.1, seq).0);
+        let aad = cipher::make_tls12_aad(seq, m.typ, m.version, payload.len());
+
+        self.0
+            .encrypt_in_place(&nonce, &aad, &mut payload)
+            .map_err(|_| rustls::Error::EncryptError)
+            .map(|_| cipher::OpaqueMessage::new(m.typ, m.version, payload))
+    }
+}
+
+impl cipher::MessageDecrypter for Tls12Cipher {
+    fn decrypt(
+        &self,
+        mut m: cipher::OpaqueMessage,
+        seq: u64,
+    ) -> Result<cipher::PlainMessage, rustls::Error> {
+        let payload = m.payload();
+        let nonce = chacha20poly1305::Nonce::from(cipher::Nonce::new(&self.1, seq).0);
+        let aad = cipher::make_tls12_aad(
+            seq,
+            m.typ,
+            m.version,
+            payload.len() - CHACHAPOLY1305_OVERHEAD,
+        );
+
+        let payload = m.payload_mut();
+        self.0
+            .decrypt_in_place(&nonce, &aad, payload)
+            .map_err(|_| rustls::Error::DecryptError)?;
+
+        Ok(m.into_plain_message())
+    }
+}
+
+const CHACHAPOLY1305_OVERHEAD: usize = 16;
--- stm32f429/src/lib.rs
+++ stm32f429/src/lib.rs
@@ -1,6 +1,11 @@
 #![no_main]
 #![no_std]
 #![feature(type_alias_impl_trait)]
+
+mod aead;
+mod hash;
+mod hmac;
+
 extern crate alloc;
 use static_cell::make_static;
 
@@ -19,7 +24,44 @@ use embassy_stm32::rng::Rng;
 use embassy_stm32::time::mhz;
 use embassy_stm32::{bind_interrupts, eth, peripherals, rng, Config};
 use embedded_alloc::Heap;
+use rustls::crypto::CryptoProvider;
 use spin;
+
+static ALL_CIPHER_SUITES: &[rustls::SupportedCipherSuite] =
+    &[TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256];
+
+pub static TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256: rustls::SupportedCipherSuite =
+    rustls::SupportedCipherSuite::Tls12(&rustls::Tls12CipherSuite {
+        common: rustls::cipher_suite::CipherSuiteCommon {
+            suite: rustls::CipherSuite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+            hash_provider: &hash::Sha256,
+        },
+        kx: rustls::crypto::KeyExchangeAlgorithm::ECDHE,
+        sign: &[
+            rustls::SignatureScheme::RSA_PSS_SHA256,
+            rustls::SignatureScheme::RSA_PKCS1_SHA256,
+        ],
+        prf_provider: &rustls::crypto::tls12::PrfUsingHmac(&hmac::Sha256Hmac),
+        aead_alg: &aead::Chacha20Poly1305,
+    });
+#[derive(Debug)]
+struct DemoCryptoProvider;
+impl CryptoProvider for DemoCryptoProvider {
+    fn fill_random(&self, bytes: &mut [u8]) -> Result<(), rustls::crypto::GetRandomFailed> {
+        use rand_core::RngCore;
+        rand_core::OsRng
+            .try_fill_bytes(bytes)
+            .map_err(|_| rustls::crypto::GetRandomFailed)
+    }
+
+    fn default_cipher_suites(&self) -> &'static [rustls::SupportedCipherSuite] {
+        ALL_CIPHER_SUITES
+    }
+
+    fn default_kx_groups(&self) -> &'static [&'static dyn rustls::crypto::SupportedKxGroup] {
+        kx::ALL_KX_GROUPS
+    }
+}
 
 const HEAP_SIZE: usize = 1024;
 #[global_allocator]
